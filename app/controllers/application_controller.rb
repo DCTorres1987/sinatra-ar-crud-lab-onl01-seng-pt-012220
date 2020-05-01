b0558e35a99267357fd74f9c03c427af1ca97971
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-05-01 23:12:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,8 @@ class ApplicationController < Sinatra::Base
   patch '/articles/:id' do 
     @article = Article.find_by(id: params["id"])
     @article.update(params[:article])
-
+     
+    redirect to ''
   end
     
   
