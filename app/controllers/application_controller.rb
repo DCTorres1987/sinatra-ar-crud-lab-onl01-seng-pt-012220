eddb9d70a3434a51031a2ae7681f4e06fa29e8a8
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-05-01 22:57:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,11 +30,11 @@ class ApplicationController < Sinatra::Base
   
   get '/articles/:id' do 
     @article = Article.find_by(id: params["id"])
-    binding.pry
     erb :show
   end
   
   get '/articles/:id/edit' do 
+    @article = Article.find_by(id: params["id"])
     erb :edit
   end
   
